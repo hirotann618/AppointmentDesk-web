84d4e4c55086bbc76699cd760b145afc8f650d4f
Hirotaka Uno <public.hirotaka@gmail.com>
2019-06-09 15:52:08 +0900

    とりあえず公開版

---
+++ public/js/index.js
@@ -0,0 +1,51 @@
+
+class Index {
+
+  static init() {
+    // Firebaseの初期化
+    var config = {
+      apiKey: "AIzaSyDKRzXfZC2R3H-aiJ8TXYtTPVOalpM-ogQ",
+      authDomain: "appointmentdesk-2a477.firebaseapp.com",
+      databaseURL: "https://appointmentdesk-2a477.firebaseio.com",
+      projectId: "appointmentdesk-2a477",
+      storageBucket: "appointmentdesk-2a477.appspot.com",
+      messagingSenderId: "806497244401",
+      appId: "1:806497244401:web:a109233e493316e2"
+    };
+    firebase.initializeApp(config);
+
+    // FirebaseUIインスタンス初期化
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    // FirebaseUIの各種設定
+    var uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return true;
+        },
+        uiShown: function() {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }
+      },
+      signInFlow: 'popup',
+      signInSuccessUrl: 'main.html',
+      signInOptions: [
+        // サポートするプロバイダを指定
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+      // Terms of service url.(サービス利用規約ページの)
+      tosUrl: '',
+      //アカウント選択を行う画面の防止
+      credentialHelper: firebaseui.auth.CredentialHelper.NONE
+    };
+
+    // FirebaseUI描画開始
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+  }
+}
---
+++ public/js/main.js
@@ -0,0 +1,118 @@
+
+class Main {
+
+    constructor() {
+        this.email = null;
+        this.uid = null;
+    }
+
+    get useremail() {
+        return this.email;
+    }
+
+    set useremail(value) {
+        this.email = value;
+    }
+
+    get useruid() {
+        return this.uid;
+    }
+
+    set useruid(value) {
+        this.uid = value;
+    }
+
+
+    static init(obj) {
+        var config = {
+            apiKey: "AIzaSyDKRzXfZC2R3H-aiJ8TXYtTPVOalpM-ogQ",
+            authDomain: "appointmentdesk-2a477.firebaseapp.com",
+            databaseURL: "https://appointmentdesk-2a477.firebaseio.com",
+            projectId: "appointmentdesk-2a477",
+            storageBucket: "appointmentdesk-2a477.appspot.com",
+            messagingSenderId: "806497244401",
+            appId: "1:806497244401:web:a109233e493316e2"
+        };
+        firebase.initializeApp(config);
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                obj.useremail = user.email;
+                obj.useruid = user.uid;
+
+                jQuery('#nav_displayName').append(user.displayName);
+
+                var db = firebase.firestore();
+
+                var dataRef = db.collection('root').doc('record').collection(user.uid);
+                dataRef.orderBy("date", "desc").onSnapshot(function (querySnapshot) {
+                    jQuery('#table').empty();
+                    querySnapshot.forEach(function (doc) {
+                        // console.log(doc.data().date);
+                        jQuery('#table').append('<tr>');
+                        jQuery('#table').append('<td onclick="main.onDelete(main.useruid,\''+ doc.id +'\')">' + doc.data().code + '</td>');
+                        jQuery('#table').append('<td onclick="main.onDelete(main.useruid,\''+ doc.id +'\')">' + moment(doc.data().date.toDate(), 'ddd MMM DD YYYY HH:mm:ss z ZZ').format('HH:mm') + '</td>');
+                        jQuery('#table').append('</tr>');
+                    });
+                });
+                
+                // usersRef.where('date', '>=', new Date(new Date().setHours(0, 0, 0, 0))).where('date', '<=', end).onSnapshot(function (querySnapshot) {
+                //     jQuery('#recode-button').empty();
+                //     if ( querySnapshot.size === 0 ){
+                //         jQuery('#recode-button').append('<button type="button" class="btn btn-primary btn-block btn-lg" onclick="main.onClick(main.useruid, \'出社\');">出社</button>');
+                //     }else if ( querySnapshot.size === 1 ){
+                //         jQuery('#recode-button').append('<button type="button" class="btn btn-primary btn-block btn-lg" onclick="main.onClick(main.useruid, \'退社\');">退社</button>');
+                //     }
+                // });
+
+            } else {
+                window.location.href = 'index.html'
+            }
+        });
+    }
+
+    onDelete(uid, id) {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top',
+            showConfirmButton: false,
+            timer: 1500
+        });
+
+        var options = {
+            title: "削除してよろしいですか。",
+            type: "question",
+            showCancelButton: true,
+        };
+
+        Swal.fire(options)
+            .then(function (isConfirm) {
+                if (isConfirm.value) {
+
+                    var db = firebase.firestore();
+                    db.collection('root').doc('record').collection(uid).doc(id).delete().then(function () {
+                        Toast.fire({
+                            type: "success",
+                            title: "削除しました。",
+                        })
+                    }).catch(function (error) {
+                        Swal.fire({
+                            type: "error",
+                            title: "エラーが発生しました。",
+                            text: error,
+                        });
+                    });
+                } else {
+                    // キャンセルボタンを押した時の処理
+                    // valには 'null' が返ってきます
+                    Swal.fire({
+                        text: "キャンセルされました",
+                        type: "info",
+                    });
+                }
+            });
+    }
+}
+
+var main = new Main();
+Main.init(main);
